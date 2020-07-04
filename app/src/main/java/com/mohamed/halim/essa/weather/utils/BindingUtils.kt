1f9167d0548856f020a963299a2fc0850b079976
MohamedHalim <mohamedhalim1998@gmail.com>
2020-07-04 20:39:24 +0200

    set the weather icon

--- app/src/main/java/com/mohamed/halim/essa/weather/utils/BindingUtils.kt
+++ app/src/main/java/com/mohamed/halim/essa/weather/utils/BindingUtils.kt
@@ -1,5 +1,6 @@
 package com.mohamed.halim.essa.weather.utils
 
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.mohamed.halim.essa.weather.R
@@ -7,29 +8,48 @@ import timber.log.Timber
 import java.text.SimpleDateFormat
 import java.util.*
 
-@BindingAdapter("setTemperature")
+@BindingAdapter("temperature")
 fun setTemperature(textView: TextView, temp: Float) {
     textView.text = textView.context.getString(R.string.temperature, temp.toInt())
 }
 
-@BindingAdapter("setTemperatureFeelsLike")
+@BindingAdapter("temperatureFeelsLike")
 fun setTemperatureFeelsLike(textView: TextView, temp: Float) {
     textView.text = textView.context.getString(R.string.feels_like, temp.toInt())
 }
 
-@BindingAdapter("setDate")
+@BindingAdapter("date")
 fun setDate(textView: TextView, timeStamp: Long) {
     val dateFormat = SimpleDateFormat("MMM d, yyyy")
     val date = Date(timeStamp * 1000)
     textView.text = dateFormat.format(date)
 }
 
-@BindingAdapter("setTemperatureMin")
+@BindingAdapter("temperatureMin")
 fun setTemperatureMin(textView: TextView, temp: Float) {
     textView.text = textView.context.getString(R.string.min, temp.toInt())
 }
 
-@BindingAdapter("setTemperatureMax")
+@BindingAdapter("temperatureMax")
 fun setTemperatureMax(textView: TextView, temp: Float) {
     textView.text = textView.context.getString(R.string.max, temp.toInt())
+}
+
+@BindingAdapter("conditionIcon")
+fun setConditionIcon(imageView: ImageView, condition: Int) {
+    imageView.setImageDrawable(
+        imageView.context.getDrawable(
+            when (condition) {
+                in 200..232 -> R.drawable.thunderstorm
+                in 300..321 -> R.drawable.heavy_rain
+                in 500..503 -> R.drawable.light_rain
+                in 521..531 -> R.drawable.heavy_rain
+                in 600..622 -> R.drawable.snow
+                in 700..781 -> R.drawable.heavy_cloud
+                800 -> R.drawable.clear
+                in 801..804 -> R.drawable.light_cloud
+                else -> R.drawable.clear
+            }
+        )
+    )
 }
